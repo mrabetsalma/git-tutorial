bcfdb687a33cb995a1c949cdcd15b10102061ac2
Lab ESPL Stud12 lab_espl_stud12 <lab_espl_stud12@tueircs-ldb02.int.rcs.ei.tum.de>
2019-11-19 15:36:24 +0100

    fixed some problems

--- src/main.c
+++ src/main.c
@@ -1,19 +1,25 @@
 #include <stdio.h> 
-#include <espl_lib.h> 
-int continue=1;
-int main () 
-{ int number;
-  char Text;
+#include "espl_lib.h" 
+
+int main ()  
+
+{ 
+  int c= 1;
+
 do{
+  unsigned int number;
+  char Text;
   printf("Hello ESPL\n");
   printf("Enter a number \n");
   scanf("%d",&number);
   getchar();
-  printf("%s",num_to_words(number));
+  printf("%c", num_to_words(number) );
   printf("\n Do you wish to continue?(Y) for yes and (N) for No? \n");
   scanf("%c",&Text);
-  if(Text=='N')
-    continue=0;
-}while(continue==1);
+  if(Text == 'N'){
+
+    c = 0;
+}
+}while(c== 1);
   
 }
