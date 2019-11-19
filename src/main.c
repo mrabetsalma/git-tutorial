ddbe6e7418daeb8c71703c06257d0151e95724fc
Lab ESPL Stud12 lab_espl_stud12 <lab_espl_stud12@tueircs-ldb02.int.rcs.ei.tum.de>
2019-11-19 19:27:09 +0100

    fix main

--- src/main.c
+++ src/main.c
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-#include <stdio.h> 
-#include "espl_lib.h" 
 
-int main ()  
-
-{ 
-  int c= 1;
-
-do{
-  unsigned int number;
-  char Text;
-  printf("Hello ESPL\n");
-  printf("Enter a number \n");
-  scanf("%d",&number);
-  getchar();
-  printf("%c", num_to_words(number) );
-  printf("\n Do you wish to continue?(Y) for yes and (N) for No? \n");
-  scanf("%c",&Text);
-  if(Text == 'N'){
-
-    c = 0;
-}
-}while(c== 1);
-  
-}
-=======
 /****************************************************************************
   ----------------------------------------------------------------------
   Copyright (C) Alexander Hoffman, 2019
@@ -245,4 +219,4 @@ void err_abort(int status, char *message) {
   exit(status);
   return ;
 }
->>>>>>> conflicts
+
