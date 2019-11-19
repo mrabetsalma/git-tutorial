de17c7fdf9495ec91cb8e1e1b4cc848087c07339
Lab ESPL Stud12 lab_espl_stud12 <lab_espl_stud12@tueircs-ldb02.int.rcs.ei.tum.de>
2019-11-19 13:00:43 +0100

    added new main function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h> 
-#include <stdlib.h> 
-
+#include <espl_lib.h> 
+int continue=1;
 int main () 
-{ 
-printf("Hello ESPL");
-return 0;
+{ int number;
+  char Text;
+do{
+  printf("Hello ESPL\n");
+  printf("Enter a number \n");
+  scanf("%d",&number);
+  getchar();
+  printf("%s",num_to_words(number));
+  printf("\n Do you wish to continue?(Y) for yes and (N) for No? \n");
+  scanf("%c",&Text);
+  if(Text=='N')
+    continue=0;
+}while(continue==1);
+  
 }
